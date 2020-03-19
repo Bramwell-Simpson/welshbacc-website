35a60764effaec5e19bf9d99a12942a9d97c6439
Bramwell-Simpson <32172769+Bramwell-Simpson@users.noreply.github.com>
2020-03-19 09:50:28 +0000

    Initial Commit
    Add main page, CSS and JS

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+// Used to toggle the menu on small screens when clicking on the menu button
+function navbar() {
+	var x = document.getElementById("navBarMobile");
+	if (x.className.indexOf("w3-show") == -1) 
+	{
+		x.className += " w3-show";
+	} 
+	else 
+	{ 
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
